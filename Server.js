0ae6a3d3cfcdd43aedfbc8b7651faf4a5a63336a
Anurag Singh <anurags9693@gmail.com>
2025-01-03 00:17:32 +0500

    01

---
+++ Server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./App');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+
+    console.log(`Server running on port ${port}`)
+
+
+} );
+
